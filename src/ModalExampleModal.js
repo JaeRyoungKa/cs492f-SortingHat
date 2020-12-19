82b9ef1bb697415c1b9077e0ce0977f153d25f28
zee2013234 <zee2013234@kaist.ac.kr>
2020-12-19 15:40:02 +0900

    UI improvements with multiple sliders
    Multiple "parameters" in contributor feedback modal, and added multiple sliders

--- src/ModalExampleModal.js
+++ src/ModalExampleModal.js
@@ -1,12 +1,38 @@
 import React from 'react'
 import { Button, Input, Modal } from 'semantic-ui-react'
 
-function maker(isContribute) {
+function maker(isContribute) { //contribution modal for contributor
   if (isContribute === "1") {
-    return (<div>        <p>  What do you think is the most appropriate translation for the word?    </p>        <Input focus color='purple' size='small' placeholder='Most appropriate one?' />      </div>
+      return (<div>        <p>  What do you think is the most appropriate translation for the word?    </p>
+          <div style={{textAlign: "center", marginTop :"10px"}}> Please choose the domain of this word </div>
+          <div><select name="domain" style={{ display: "block", margin: "0 auto", textAlign: "center" }}>
+              <option value="default"> General </option>
+              <option value="math"> Mathematics </option>
+              <option value="sociology">Sociology </option>
+          </select></div>
+          <div style={{ textAlign: "center", marginTop: "20px" }}> Please choose appropriate audience for this suggestion </div>
+          <div><select name="domain" style={{ display: "block", margin: "0 auto", textAlign: "center" }}>
+              <option value="kindergarten"> Kindergarten </option>
+              <option value="elementary"> Elementary School </option>
+              <option value="middle">Middle School </option>
+              <option value="high">High School </option>
+              <option value="college"> College and above </option>  
+          </select></div>
+          <div style={{ textAlign: "center", marginTop: "20px" }}> Please write down the most appropriate translation for this word </div>
+          <Input focus color='purple' size='small' placeholder='Most appropriate one?' style={{ display: "block", margin: "0 auto", textAlign: "center" }} /><div />
+          <div style={{ textAlign: "center", marginTop: "20px" }}> Please choose the level of your expertise </div>
+          <div><select name="domain" style={{ display: "block", margin: "0 auto", textAlign: "center" }}>
+              <option value="bachelor"> Bachelor </option>
+              <option value="master"> Master </option>
+              <option value="phd">Ph.D </option>
+              <option value="expert">Expert </option>
+          </select></div>     </div>
     )
   } else if (isContribute === "0") {
-    return ( <div>  <p>  We will let experts take a look at your suggestion and reflect in the future translation model.    </p>        <p>          If you have a keyword to suggest in your mind, please let me know what you think the best suitable one is.</p>  <Input focus color='purple' size='small' style={{width:"300px"}} placeholder='Most appropriate one? (Optional)'/></div>)
+    return ( <div>  <p>  We will let experts take a look at your suggestion and reflect in the future translation model.    </p>
+              <p>
+                  If you have a keyword to suggest in your mind, please let me know what you think the best suitable one is.</p>
+              <Input focus color='purple' size='small' style={{ width: "300px" }} placeholder='Most appropriate one? (Optional)' /></div>)
   } else {
     return (
       <div>
--- src/contribute.js
+++ src/contribute.js
@@ -5,7 +5,7 @@ import ModalExampleModal from './ModalExampleModal'
 class Contribute extends Component {
     constructor(props) {
         super(props);
-        this.state = { value: "25", showSetting: false, doSuggest: false, isVisible: false, word: "None", isModalVisible: false, activeItem: '1' };
+        this.state = { value1: "25", value2: "50", value3: "20", value4: "50", showSetting: false, doSuggest: false, isVisible: false, word: "None", isModalVisible: false, activeItem: '1' };
         this.report = this.report.bind(this);
         this.ptr = 1
         this.word1 = {
@@ -82,7 +82,83 @@ class Contribute extends Component {
         }
     }
 
-    handleOnChange = (e) => this.setState({ value: e.target.value })
+    setting1 = (value) => {
+        if (value >= parseInt("0", 10) && value <= parseInt("33", 10)) {
+            return (<p>General</p>)
+        }
+        else if (value > parseInt("33", 10) && value <= parseInt("67", 10)) {
+            return (<p>Mathematics</p>)
+        }
+        else {
+            return (<p>Sociology</p>)
+        }
+    }
+    setting2 = (value) => {
+        if (value >= parseInt("0", 10) && value <= parseInt("12", 10)) {
+            return (<p>Kindergarten</p>)
+        }
+        else if (value > parseInt("12", 10) && value <= parseInt("25", 10)) {
+            return (<p>Elementary School</p>)
+        }
+        else if (value > parseInt("25", 10) && value <= parseInt("37", 10)) {
+            return (<p>Middle School</p>)
+        }
+        else if (value > parseInt("37", 10) && value <= parseInt("50", 10)) {
+            return (<p>High School</p>)
+        }
+        else if (value > parseInt("50", 10) && value <= parseInt("62", 10)) {
+            return (<p>Bachelor</p>)
+        }
+        else if (value > parseInt("62", 10) && value <= parseInt("75", 10)) {
+            return (<p>Master</p>)
+        }
+        else if (value > parseInt("75", 10) && value <= parseInt("87", 10)) {
+            return (<p>Ph.D</p>)
+        }
+        else {
+            return (<p>PostDoc</p>)
+        }
+    }
+    setting3 = (value) => {
+        if (value >= parseInt("0", 10) && value <= parseInt("20", 10)) {
+            return (<p>Bachelor</p>)
+        }
+        else if (value > parseInt("20", 10) && value <= parseInt("40", 10)) {
+            return (<p>Master</p>)
+        }
+        else if (value > parseInt("40", 10) && value <= parseInt("60", 10)) {
+            return (<p>Ph.D</p>)
+        }
+        else if (value > parseInt("60", 10) && value <= parseInt("80", 10)) {
+            return (<p>PostDoc</p>)
+        }
+        else {
+            return (<p>Expert</p>)
+        }
+    }
+    setting4 = (value) => {
+        if (value >= parseInt("0", 10) && value <= parseInt("20", 10)) {
+            return (<p>Kindergarten</p>)
+        }
+        else if (value > parseInt("20", 10) && value <= parseInt("40", 10)) {
+            return (<p>Elementary School</p>)
+        }
+        else if (value > parseInt("40", 10) && value <= parseInt("60", 10)) {
+            return (<p>Middle School</p>)
+        }
+        else if (value > parseInt("60", 10) && value <= parseInt("80", 10)) {
+            return (<p>High School</p>)
+        }
+        else {
+            return (<p>College and Above</p>)
+        }
+    }
+
+
+    handleOnChange1 = (e) => this.setState({ value1: e.target.value })
+    handleOnChange2 = (e) => this.setState({ value2: e.target.value })
+    handleOnChange3 = (e) => this.setState({ value3: e.target.value })
+    handleOnChange4 = (e) => this.setState({ value4: e.target.value })
 
     render() {
         const activeItem = this.state.activeItem
@@ -291,8 +367,27 @@ class Contribute extends Component {
                         <Segment color="violet" textAlign="center">
                             <h3>Try adjusting the parameter to change the setting</h3>
                             <div>
-                                <input type="range" min="0" max="100" value={this.state.value} onChange={this.handleOnChange} />
-                                <div className="value">{this.state.value}</div>
+                                <div>
+                                    <p><b> Domain of the Excerpt </b></p>
+                                    <input type="range" min="0" max="100" value={this.state.value1} onChange={this.handleOnChange1} />
+                                    <div className="value">{this.setting1(this.state.value1)}</div>
+                                </div>
+                                <div style={{ marginTop: "30px" }}>
+                                    <p><b> Peer Suggestion </b></p>
+                                    <input type="range" min="0" max="100" value={this.state.value2} onChange={this.handleOnChange2} />
+                                    <div className="value">{this.setting2(this.state.value2)}</div>
+                                </div>
+                                <div style={{ marginTop: "30px" }}>
+                                    <p><b> Experts' Suggestion </b></p>
+                                    <input type="range" min="0" max="100" value={this.state.value3} onChange={this.handleOnChange3} />
+                                    <div className="value">{this.setting3(this.state.value3)}</div>
+                                </div>
+                                <div style={{ marginTop: "30px" }}>
+                                    <p><b> Your Educational Background </b></p>
+                                    <p><i> Disclaimer: This is to provide personal suggestion tailored to your educational background, and moving this slider does not collect your personal information</i></p>
+                                    <input type="range" min="0" max="100" value={this.state.value4} onChange={this.handleOnChange4} />
+                                    <div className="value">{this.setting4(this.state.value4)}</div>
+                                </div>
                             </div>
 
 
